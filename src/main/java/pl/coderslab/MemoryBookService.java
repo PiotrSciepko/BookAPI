670e6ea1e818a5b02b26b9394accf6243db214e9
PiotrSciepko <piotrek.priv@gmail.com>
2020-11-06 20:50:47 +0100

    modify MemoryBookService.java

--- src/main/java/pl/coderslab/MemoryBookService.java
+++ src/main/java/pl/coderslab/MemoryBookService.java
@@ -11,7 +11,8 @@ import java.util.stream.Collectors;
 //@Component
 @Service
 public class MemoryBookService {
-    private static List<Book> books = new ArrayList<>(Arrays.asList(new Book(1L, "9788324631766", "Thinking in Java", "Bruce Eckel", "Helion", "programming"),
+    private static List<Book> books = new ArrayList<>(Arrays.asList(
+            new Book(1L, "9788324631766", "Thinking in Java", "Bruce Eckel", "Helion", "programming"),
             new Book(2L, "1234567891234", "Pan Tadeusz", "Adam Mickiewicz", "Libro", "classic"),
             new Book(3L, "9876543219876", "Mein Kampf", "Adolf Hitler", "Axel Springer", "military"),
             new Book(4L, "1928374656473", "Pan Wołodyjowski", "Henryk Sienkiewicz", "PWN", "history"),
